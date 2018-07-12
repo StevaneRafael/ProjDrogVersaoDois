8a9d3c1226751333700226a35b221813d7834627
StevaneRafael <stevanerafael@gmail.com>
2018-07-12 14:22:52 -0300

    16ª versao (EstadoDAO - editar-156)

--- src/test/java/br/com/rafael/drogariavd/dao/EstadoDAOTest.java
+++ src/test/java/br/com/rafael/drogariavd/dao/EstadoDAOTest.java
@@ -50,6 +50,7 @@ public class EstadoDAOTest {
 	}
 	
 	@Test
+	@Ignore
 	public void excluir() {
 		Long codigo = 4L;		
 		EstadoDAO estadoDAO = new EstadoDAO();
@@ -61,6 +62,28 @@ public class EstadoDAOTest {
 			estadoDAO.excluir(estado);						
 			System.out.println("Registro removido: ");
 			System.out.println(estado.getSigla() + " - " + estado.getNome());
+		}
+	}
+	
+	
+	@Test
+	public void editar() {
+		Long codigo = 3L;		
+		EstadoDAO estadoDAO = new EstadoDAO();
+		Estado estado = estadoDAO.buscar(codigo);
+		
+		if(estado == null) {
+			System.out.println("Nenhum registro encontrado!");
+		} else {
+			System.out.println("Registro editado - Antes: ");
+			System.out.println(estado.getCodigo() + " - " + estado.getSigla() + " - " + estado.getNome());
+			
+			estado.setNome("Minas Gerais");
+			estado.setSigla("MG");
+			estadoDAO.editar(estado);
+
+			System.out.println("Registro editado - depois: ");
+			System.out.println(estado.getCodigo() + " - " + estado.getSigla() + " - " + estado.getNome());
 		}
 	}
 }
