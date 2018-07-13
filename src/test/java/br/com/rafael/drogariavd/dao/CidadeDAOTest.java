715d8b877b3fb7692c04c0e3cb8c68b6f0234511
StevaneRafael <stevanerafael@gmail.com>
2018-07-13 14:10:04 -0300

    19ª versao (CidadeDAO - Listar e Buscar - 159)

--- src/test/java/br/com/rafael/drogariavd/dao/CidadeDAOTest.java
+++ src/test/java/br/com/rafael/drogariavd/dao/CidadeDAOTest.java
@@ -1,5 +1,7 @@
 package br.com.rafael.drogariavd.dao;
 
+import java.util.List;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -22,5 +24,41 @@ public class CidadeDAOTest {
 		
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		cidadeDAO.salvar(cidade);
+	}
+	
+	@Test
+	@Ignore
+	public void listar() {
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		List<Cidade> resultado = cidadeDAO.listar();
+		
+		for(Cidade cidade : resultado) {
+			System.out.println("Código da cidade: " + cidade.getCodigo());
+			System.out.println("Nome da Cidade: " + cidade.getNome());
+			System.out.println("Código do Estado: " + cidade.getEstado().getCodigo());			
+			System.out.println("Sigla do Estado: " + cidade.getEstado().getSigla());
+			System.out.println("Nome do Estado: " + cidade.getEstado().getNome());
+				
+			System.out.println();
+			
+		}
+	}
+	
+	@Test
+	public void buscar() {
+		Long codigo = 2L;
+		
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		Cidade cidade = cidadeDAO.buscar(codigo);
+	
+	
+		System.out.println("Código da cidade: " + cidade.getCodigo());
+		System.out.println("Nome da Cidade: " + cidade.getNome());
+		System.out.println("Código do Estado: " + cidade.getEstado().getCodigo());			
+		System.out.println("Sigla do Estado: " + cidade.getEstado().getSigla());
+		System.out.println("Nome do Estado: " + cidade.getEstado().getNome());
+		
+		System.out.println();
+		
 	}
 }
