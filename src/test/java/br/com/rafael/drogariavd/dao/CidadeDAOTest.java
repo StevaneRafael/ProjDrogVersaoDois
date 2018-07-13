c07f5881fc708ef0eb535e30426b261a76e0a63d
StevaneRafael <stevanerafael@gmail.com>
2018-07-13 14:29:34 -0300

    20ª versao (CidadeDAO - Excluir - 160)

--- src/test/java/br/com/rafael/drogariavd/dao/CidadeDAOTest.java
+++ src/test/java/br/com/rafael/drogariavd/dao/CidadeDAOTest.java
@@ -15,50 +15,70 @@ public class CidadeDAOTest {
 	@Ignore
 	public void salvar() {
 		EstadoDAO estadoDAO = new EstadoDAO();
-		
+
 		Estado estado = estadoDAO.buscar(1L);
-		
+
 		Cidade cidade = new Cidade();
 		cidade.setNome("Rio de Janeiro");
 		cidade.setEstado(estado);
-		
+
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		cidadeDAO.salvar(cidade);
 	}
-	
+
 	@Test
 	@Ignore
 	public void listar() {
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		List<Cidade> resultado = cidadeDAO.listar();
-		
-		for(Cidade cidade : resultado) {
+
+		for (Cidade cidade : resultado) {
 			System.out.println("Código da cidade: " + cidade.getCodigo());
 			System.out.println("Nome da Cidade: " + cidade.getNome());
-			System.out.println("Código do Estado: " + cidade.getEstado().getCodigo());			
+			System.out.println("Código do Estado: " + cidade.getEstado().getCodigo());
 			System.out.println("Sigla do Estado: " + cidade.getEstado().getSigla());
 			System.out.println("Nome do Estado: " + cidade.getEstado().getNome());
-				
+
 			System.out.println();
-			
+
 		}
 	}
-	
+
 	@Test
+	@Ignore
 	public void buscar() {
 		Long codigo = 2L;
-		
+
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		Cidade cidade = cidadeDAO.buscar(codigo);
-	
-	
+
 		System.out.println("Código da cidade: " + cidade.getCodigo());
 		System.out.println("Nome da Cidade: " + cidade.getNome());
-		System.out.println("Código do Estado: " + cidade.getEstado().getCodigo());			
+		System.out.println("Código do Estado: " + cidade.getEstado().getCodigo());
 		System.out.println("Sigla do Estado: " + cidade.getEstado().getSigla());
 		System.out.println("Nome do Estado: " + cidade.getEstado().getNome());
-		
+
 		System.out.println();
-		
+
 	}
+
+	@Test
+	public void excluir() {
+		Long codigo = 4L;
+
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		Cidade cidade = cidadeDAO.buscar(codigo);
+		
+		cidadeDAO.excluir(cidade);
+		
+		System.out.println("Cidade Removida:");
+		System.out.println("Código da cidade: " + cidade.getCodigo());
+		System.out.println("Nome da Cidade: " + cidade.getNome());
+		System.out.println("Código do Estado: " + cidade.getEstado().getCodigo());
+		System.out.println("Sigla do Estado: " + cidade.getEstado().getSigla());
+		System.out.println("Nome do Estado: " + cidade.getEstado().getNome());
+
+		System.out.println();
+	}
+
 }
