940d0f5d55e76fedaee58f180d2bdebbe59f8cc0
StevaneRafael <stevanerafael@gmail.com>
2018-07-18 13:44:30 -0300

    34ª versao (Introducao ao OmniFaces - 177)

--- src/main/java/br/com/rafael/drogariavd/bean/EstadoBean.java
+++ src/main/java/br/com/rafael/drogariavd/bean/EstadoBean.java
@@ -1,16 +1,20 @@
 package br.com.rafael.drogariavd.bean;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
+
+import org.omnifaces.util.Messages;
 
 @ManagedBean
 public class EstadoBean {
 	public void salvar() {
+		/* Metódo mais 'Antigo'
 		String texto = "Programação Web com Java";
 		FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_ERROR, texto, texto);
 		
 		FacesContext contexto =  FacesContext.getCurrentInstance();
 		contexto.addMessage(null, mensagem);
+		*/
+		Messages.addGlobalInfo("Programação Web com Java");
+		
 	}
 }
