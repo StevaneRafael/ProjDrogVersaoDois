2abe7d7595a5b476d3941abfdfc7dcf17f4202b6
StevaneRafael <stevanerafael@gmail.com>
2018-08-06 17:10:13 -0300

    50ª versao (CidadeBean - Editar - 195)

--- src/main/java/br/com/rafael/drogariavd/bean/CidadeBean.java
+++ src/main/java/br/com/rafael/drogariavd/bean/CidadeBean.java
@@ -104,4 +104,16 @@ public class CidadeBean implements Serializable {
 			erro.printStackTrace();
 		}
 	}
+	
+	public void editar(ActionEvent evento){
+		try {
+			cidade = (Cidade) evento.getComponent().getAttributes().get("cidadeSelecionada");
+
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar selecionar uma cidade");
+			erro.printStackTrace();
+		}	
+	}
 }
