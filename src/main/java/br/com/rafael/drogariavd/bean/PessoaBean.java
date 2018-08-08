d6c74722537c865cfa1a8301b00bb637afeb0ad7
StevaneRafael <stevanerafael@gmail.com>
2018-08-08 14:06:58 -0300

    60ª versao (PessoaBean - Novo - 204)

--- src/main/java/br/com/rafael/drogariavd/bean/PessoaBean.java
+++ src/main/java/br/com/rafael/drogariavd/bean/PessoaBean.java
@@ -1,6 +1,7 @@
 package br.com.rafael.drogariavd.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -10,7 +11,10 @@ import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
+import br.com.rafael.drogariavd.dao.EstadoDAO;
 import br.com.rafael.drogariavd.dao.PessoaDAO;
+import br.com.rafael.drogariavd.domain.Cidade;
+import br.com.rafael.drogariavd.domain.Estado;
 import br.com.rafael.drogariavd.domain.Pessoa;
 
 @SuppressWarnings("serial")
@@ -19,36 +23,66 @@ import br.com.rafael.drogariavd.domain.Pessoa;
 public class PessoaBean implements Serializable {
 	private Pessoa pessoa;
 	private List<Pessoa> pessoas;
-	
+
+	private List<Estado> estados;
+	private List<Cidade> cidades;
+
 	public Pessoa getPessoa() {
 		return pessoa;
 	}
-	
+
 	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
 	}
-	
+
 	public List<Pessoa> getPessoas() {
 		return pessoas;
 	}
-	
+
 	public void setPessoas(List<Pessoa> pessoas) {
 		this.pessoas = pessoas;
 	}
-	
+
+	public List<Estado> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
+
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+
 	@PostConstruct
 	public void listar() {
-		try{
+		try {
 			PessoaDAO pessoaDAO = new PessoaDAO();
 			pessoas = pessoaDAO.listar();
-		}catch(RuntimeException erro){
+		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Ocorreu um erro ao tentar listar as pessoas");
 			erro.printStackTrace();
 		}
 	}
 
 	public void novo() {
+		try {
+			pessoa = new Pessoa();
+			
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+			
+			cidades = new ArrayList<Cidade>();
 
+		}catch(RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar carregar os estados");
+			erro.printStackTrace();
+		}
 	}
 
 	public void editar(ActionEvent evento) {
